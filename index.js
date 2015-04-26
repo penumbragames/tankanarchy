d7e0d4892d08c86e65dac7a1f33452d78171f8c7
Alvin Lin <alvin.lin@stuypulse.com>
2015-04-26 17:01:24 -0400

    base files

---
+++ index.js
@@ -0,0 +1,13 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+});
+
+http.listen(3000, function() {
+});
