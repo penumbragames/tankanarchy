d63fe1bd666d13982815dddf874b3086fcb982df
Kenneth Li <noobbyte@gmail.com>
2015-05-23 17:16:43 -0400

    Turret turns

--- public/game/Drawing.js
+++ public/game/Drawing.js
@@ -11,9 +11,13 @@ Drawing.prototype.drawSelf = function(coords, orientation, turretAngle) {
   this.context_.save();
   this.context_.translate(coords[0], coords[1]);
   this.context_.rotate(orientation);
-  var img = new Image();
-  img.src = '../data/self_tank.png';
-  this.context_.drawImage(img, -15, -25);
+  var tank = new Image();
+  tank.src = '../data/self_tank.png';
+  this.context_.drawImage(tank, -15, -25);
+  this.context_.rotate(turretAngle);
+  var turret = new Image();
+  turret.src = '../data/self_tank_turret.png';
+  this.context_.drawImage(turret, -15, -25);
   this.context_.restore();
 };
 
@@ -21,8 +25,12 @@ Drawing.prototype.drawOther = function(coords, orientation, turretAngle) {
   this.context_.save();
   this.context_.translate(coords[0], coords[1]);
   this.context_.rotate(orientation);
-  var img = new Image();
-  img.src = '../data/other_tank.png';
-  this.context_.drawImage(img, -15, -25);
+  var tank = new Image();
+  tank.src = '../data/other_tank.png';
+  this.context_.drawImage(tank, -15, -25);
+  this.context_.rotate(turretAngle);
+  var turret = new Image();
+  turret.src = '../data/other_tank_turret.png';
+  this.context_.drawImage(turret, -15, -25);
   this.context_.restore();
 };
