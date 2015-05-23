41e04dcc132dd848fa162c0f4f0bdcb1c6888424
Kenneth Li <noobbyte@gmail.com>
2015-05-23 13:40:18 -0400

    Viewport work

--- public/game/ViewPort.js
+++ public/game/ViewPort.js
@@ -5,3 +5,20 @@
 function ViewPort() {
 
 }
+
+ViewPort.locate = function(game, player) {
+  var players = game.players_;
+  var id = player.getID();
+  var onScreen = [player];
+
+  for (var i = 0; i < players.length; i++) {
+    var curPlayer = players[i]
+    if (curPlayer.id_ != id) {
+      if (Math.abs(curPlayer.getX() - player.getX()) < 400 && 
+          Math.abs(curPlayer.getY() - player.getY()) < 300)
+        onScreen.push(curPlayer);
+    }
+  }
+  return onScreen;
+}
+  
