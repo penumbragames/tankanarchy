76a2719f78d23a211f83d2c6523d9d554cc0f8d5
Alvin Lin <omgimanerd@yahoo.com>
2015-06-17 16:18:02 -0400

    kill announcer class

---
+++ public/game/Kill_Announcer.js
@@ -0,0 +1,14 @@
+/**
+ * This class manages the kill messages at the top of the screen.
+ * When a player is killed, the corresponding message will be
+ * displayed.
+ * @author Alvin Lin (alvin.lin@stuypulse.com)
+ */
+
+/**
+ * @param {Element} feed The element that will serve as the container
+ *   for the kill messages.
+ */
+function Kill_Announcer(feed) {
+  this.feed = feed;
+};
