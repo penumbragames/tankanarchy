29252ddb3d845d89609aa827a2bdf00e5c3a7477
Alvin Lin <alvin.lin@stuypulse.com>
2015-10-09 00:02:03 -0400

    remove unneeded dependency

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var PORT_NUMBER = process.env.PORT || 5000;
 var FRAME_RATE = 1000.0 / 60.0;
 
 // Dependencies.
-var async = require('async');
 var bodyParser = require('body-parser');
 var express = require('express');
 var http = require('http');
