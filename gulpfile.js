22aed562988479325ff33d57ad3b6b0c8cd6ddcb
Alvin Lin <alvin.lin.dev@gmail.com>
2016-01-27 14:46:27 -0500

    updated gulp and project dependencies for new less compiler

--- gulpfile.js
+++ gulpfile.js
@@ -3,52 +3,52 @@
  * @author Alvin Lin (alvin.lin.dev@gmail.com)
  */
 
-var gulp = require('gulp');
+var gulp = require("gulp");
 
-var autoprefixer = require('gulp-autoprefixer');
-var closureCompiler = require('gulp-closure-compiler');
-var less = require('gulp-less');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
+var autoprefixer = require("gulp-autoprefixer");
+var closureCompiler = require("gulp-closure-compiler");
+var cssnano = require("gulp-cssnano");
+var less = require("gulp-less");
+var rename = require("gulp-rename");
 
-gulp.task('default', ['js', 'less']);
+gulp.task("default", ["js", "less"]);
 
-gulp.task('js', function() {
-  return gulp.src(['./shared/*.js',
-                   './static/js/game/*.js',
-                   './static/js/*.js'])
+gulp.task("js", function() {
+  return gulp.src(["./shared/*.js",
+                   "./static/js/game/*.js",
+                   "./static/js/*.js"])
     .pipe(closureCompiler({
-      compilerPath: 'bower_components/closure-compiler/compiler.jar',
-      fileName: 'minified.js'
+      compilerPath: "bower_components/closure-compiler/compiler.jar",
+      fileName: "minified.js"
     }))
-    .pipe(gulp.dest('./static/dist'));
+    .pipe(gulp.dest("./static/dist"));
 });
 
-gulp.task('less', function() {
-  return gulp.src('./static/less/styles.less')
+gulp.task("less", function() {
+  return gulp.src("./static/less/styles.less")
     .pipe(less({ compress: true}))
     .pipe(autoprefixer())
-    .pipe(minifyCss())
+    .pipe(cssnano())
     .pipe(rename(function(path) {
-      path.basename = 'minified';
-      path.extname = '.css';
+      path.basename = "minified";
+      path.extname = ".css";
     }))
-    .pipe(gulp.dest('./static/dist'));
+    .pipe(gulp.dest("./static/dist"));
 });
 
-gulp.task('watch-js', function() {
-  gulp.watch(['./shared/*.js',
-              './static/js/*.js',
-              './static/js/game/*.js'], ['js']);
+gulp.task("watch-js", function() {
+  gulp.watch(["./shared/*.js",
+              "./static/js/*.js",
+              "./static/js/game/*.js"], ["js"]);
 });
 
-gulp.task('watch-less', function() {
-  gulp.watch('./static/less/*.less', ['less']);
+gulp.task("watch-less", function() {
+  gulp.watch("./static/less/*.less", ["less"]);
 });
 
-gulp.task('watch', function() {
-  gulp.watch(['./shared/*.js',
-              './static/js/*.js',
-              './static/js/game/*.js'], ['js']);
-  gulp.watch('./static/less/*.less', ['less']);
+gulp.task("watch", function() {
+  gulp.watch(["./shared/*.js",
+              "./static/js/*.js",
+              "./static/js/game/*.js"], ["js"]);
+  gulp.watch("./static/less/*.less", ["less"]);
 });
