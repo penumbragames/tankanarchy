f64e0b04a2efb6c6aa951a4809e1457dd6766f14
omgimanerd <alvin@omgimanerd.tech>
2019-03-09 08:05:13 -0500

    Implemented baseline Vector class and ported Entity class to ES6

---
+++ shared/Vector.js
@@ -0,0 +1,130 @@
+/**
+ * @fileoverview Vector class for simple physics manipulations needed by the
+ *   game.
+ * @author alvin@omgimanerd.tech (Alvin Lin)
+ */
+
+/**
+ * Vector class for handling physics manipulations.
+ */
+class Vector {
+  /**
+   * Constructor for a Vector object.
+   * @param {number} [x=0] x component
+   * @param {number} [y=0] y component
+   */
+  constructor(x = 0, y = 0) {
+    this.x = x
+    this.y = y
+  }
+
+  /**
+   * Creates a new Vector object from another object.
+   * @param {Object} obj The source object
+   * @return {Vector}
+   */
+  static fromObject(obj) {
+    return new Vector(obj.x, obj.y)
+  }
+
+  /**
+   * Creates a new Vector object from an array.
+   * @param {Array<number>} array The source array
+   * @return {Vector}
+   */
+  static fromArray(array) {
+    return new Vector(array[0], array[1])
+  }
+
+  /**
+   * Return the angle of the Vector.
+   * @return {number}
+   */
+  get angle() {
+    return Math.atan2(this.y, this.x)
+  }
+
+  /**
+   * Return a copy of the negated Vector.
+   * @return {Vector}
+   */
+  get neg() {
+    return new Vector(-this.x, -this.y)
+  }
+
+  /**
+   * Return the Vector as an array representation.
+   * @return {Array<number>}
+   */
+  get asArray() {
+    return [this.x, this.y]
+  }
+
+  /**
+   * Adds two Vectors and returns a new Vector.
+   * @param {Vector} v1 The first Vector addend
+   * @param {Vector} v2 The second Vector addend
+   * @return {Vector}
+   */
+  static add(v1, v2) {
+    return new Vector(v1.x + v2.x, v1.y + v2.y)
+  }
+
+  /**
+   * Scales a Vector by a constant and returns a new Vector.
+   * @param {Vector} v The Vector to scale
+   * @param {number} c The constant scalar
+   * @return {Vector}
+   */
+  static scale(v, c) {
+    return new Vector(v.x * c, v.y * c)
+  }
+
+  /**
+   * Subtracts two Vectors and returns a new Vector.
+   * @param {Vector} v1 The Vector minuend
+   * @param {[type]} v2 The Vector subtrahend
+   * @return {Vector}
+   */
+  static sub(v1, v2) {
+    return new Vector(v1.x - v2.x, v1.y - v2.y)
+  }
+
+  /**
+   * Add another Vector to this Vector.
+   * @param {Vector} other The other vector to add
+   */
+  add(other) {
+    this.x += other.x
+    this.y += other.y
+  }
+
+  /**
+   * Returns the dot product of this Vector with another Vector.
+   * @param {Vector} other The Vector to compute the dot product with
+   * @return {number}
+   */
+  dot(other) {
+    return this.x * other.x + this.y + other.y
+  }
+
+  /**
+   * Scales this Vector by a constant scalar.
+   * @param {number} scalar The constant to scale by
+   */
+  scale(scalar) {
+    this.x *= scalar
+    this.y *= scalar
+  }
+
+  /**
+   * Subtracts another Vector from this Vector.
+   * @param {Vector} other The other Vector to subtract
+   */
+  sub(other) {
+    this.x -= other.x
+    this.y -= other.y
+  }
+}
+
+module.exports = Vector
