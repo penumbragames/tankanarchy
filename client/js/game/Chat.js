3c44d733cb66a54172b84559e11331ffa553f7a0
omgimanerd <alvin@omgimanerd.tech>
2019-03-11 21:14:24 -0400

    Update Chat.js to ES6 class syntax

--- client/js/game/Chat.js
+++ client/js/game/Chat.js
@@ -4,77 +4,76 @@
  * @author alvin.lin.dev@gmail.com (Alvin Lin)
  */
 
-/**
- * Constructor for the Chat class.
- * @constructor
- * @param {Object} socket The socket connected to the server.
- * @param {Element} displayElement The element in which the chat will be
- *   displayed.
- * @param {Element} textElement The input element from which text will be read
- *   to be sent as a chat message to the server.
- */
-function Chat(socket, displayElement, textElement) {
-  this.socket = socket;
+const Constants = require('../../shared/Constants')
 
-  this.displayElement = displayElement;
-  this.textElement = textElement;
+/**
+ * Chat class.
+ */
+class Chat {
+  /**
+   * Constructor for the Chat class, which handles sending and receiving chat
+   * from the clients.
+   * @param {Socket} socket The socket connected to the server
+   * @param {Element} displayElement The element in which the chat will
+   *   displayed
+   * @param {Element} inputElement The input element from which chat will be
+   *   read to be sent to the server
+   */
+  constructor(socket, displayElement, inputElement) {
+    this.socket = socket
+    this.displayElement = displayElement
+    this.inputElement = inputElement
+  }
+
+  /**
+   * Factory method to create a Chat class.
+   * @param {Socket} socket The socket connected to the server
+   * @param {Element} displayElementID The ID of the display element
+   * @param {Element} inputElementID The ID of the input element
+   * @return {Chat}
+   */
+  static create(socket, displayElementID, inputElementID) {
+    const displayElement = document.getElementByID(displayElementID)
+    const inputElement = document.getElementByID(inputElementID)
+    const chat = new Chat(socket, displayElement, inputElement)
+    chat.init()
+    return chat
+  }
+
+  /**
+   * Binds the event handlers to initialize the Chat class.
+   */
+  init() {
+    this.textElement.addEventListener('keydown', this.onInputKeyDown.bind(this))
+    this.socket.on(Constants.SOCKET_CHAT_SERVER_CLIENT,
+      this.onChatReceive.bind(this))
+  }
+
+  /**
+   * Event handler for a key down event on the input chat element.
+   * @param {Event} event The event passed to the event handler
+   */
+  onInputKeyDown(event) {
+    if (event.keyCode === 13) {
+      const text = this.textElement.value
+      this.textElement.value = ''
+      this.socket.emit(Constants.SOCKET_CHAT_CLIENT_SERVER, text)
+    }
+  }
+
+  /**
+   * Event handler for a socket message received for a chat message.
+   * @param {Object} data The data sent from the server
+   */
+  onChatReceive(data) {
+    const element = document.createElement('li')
+    if (data.isNotification) {
+      element.setAttribute('class', 'notification')
+    }
+    element.appendChild(
+      document.createTextNode(`${data.name}: ${data.message}`))
+    this.displayElement.appendChild(element)
+  }
 }
 
-/**
- * Factory method to create a Chat object.
- * @param {Object} socket The socket connected to the server.
- * @param {Element} displayElement The element in which the chat will be
- *   displayed.
- * @param {Element} textElement The input element from which text will be read
- *   to be sent as a chat message to the server.
- * @return {Chat}
- */
-Chat.create = function(socket, displayElement, textElement) {
-  var chat = new Chat(socket, displayElement, textElement);
-  chat.init();
-  return chat;
-};
-
-/**
- * Binds the event handlers. This should be called during the initialization
- * in client.js.
- */
-Chat.prototype.init = function() {
-  this.textElement.addEventListener('keydown', bind(this, function(e) {
-    if (e.keyCode == 13) {
-      this.sendMessage();
-    }
-  }));
-
-  this.socket.on('chat-server-to-clients', bind(this, function(data) {
-    this.receiveMessage(data['name'], data['message'], data['isNotification']);
-  }));
-};
-
-/**
- * This is called when a message is received, and will display the new
- * received message.
- * @param {string} name The name of the message sender.
- * @param {string} message The content of the message.
- * @param {boolean} isNotification Whether or not this message is an
- *   administrative notification.
- */
-Chat.prototype.receiveMessage = function(name, message, isNotification) {
-  var element = document.createElement('li');
-  if (isNotification) {
-    element.setAttribute('class', 'notification');
-  }
-  element.appendChild(document.createTextNode(name + ': ' + message));
-  this.displayElement.appendChild(element);
-};
-
-/**
- * This is called when the user presses enter in the chatbox, and takes care
- * of taking the message they typed and sending it to the server to be relayed
- * to other clients.
- */
-Chat.prototype.sendMessage = function() {
-  var text = this.textElement.value;
-  this.textElement.value = '';
-  this.socket.emit('chat-client-to-server', text);
-};
+module.exports = Chat
